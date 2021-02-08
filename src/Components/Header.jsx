7e6bb39faed5611fff2ee363ce5019e31962e9a1
Ewok1ng <joker187566@gmail.com>
2021-02-08 20:21:57 +0300

    start

---
+++ src/Components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className='header'>
+      <a href="#" className="header__logo">SN</a>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/Components/Profile.jsx
@@ -0,0 +1,22 @@
+const Profile = () => {
+  return (
+    <div className='content'>
+      <img className="background"
+           src="https://images.pexels.com/photos/1563356/pexels-photo-1563356.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+           alt=""/>
+      Main content
+      <div>Avatar + description</div>
+      <div>
+        My posts
+        <div>
+          new post
+        </div>
+        <div>
+          post 1
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Profile
